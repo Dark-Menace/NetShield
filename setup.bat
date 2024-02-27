a1664ce6f0cc8a56257c8010edcf6595a9695f33
Abhinava Bayary <109851644+Dark-Menace@users.noreply.github.com>
2024-02-28 00:50:14 +0530

    batch file upload
    A  batch file to set up the environment in a windows system.

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+:: Create or update .env file
+echo SERVER_IP="<Enter the server_ip>" > .env
+echo PORT=3000 >> .env
+
+:: Create or update other text files
+copy nul rate_limit.txt
+copy nul trace.json
+echo <Enter the client_ip> >> valid_ip.txt
+
+:: Additional setup commands can be added as needed
+
+echo Setup complete!
