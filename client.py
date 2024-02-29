791ab1f5ec317d57eb68312304ae391c364838b9
Abhinava Bayary <109851644+Dark-Menace@users.noreply.github.com>
2024-03-01 00:20:11 +0530

    Added comments to client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+"""Script to be executed on a client system"""
+
 import socket
 from dotenv import dotenv_values
 import argparse
@@ -5,13 +7,14 @@ import pyfiglet
 out=pyfiglet.figlet_format("CLIENT",font="rectangles")
 
 class Client:
-    def __init__(self):
+    def __init__(self): #initialisation constructor to get values from .env
         config=dotenv_values(".env")
         self.server_ip=config["SERVER_IP"]
         self.port=int(config["PORT"])
         self.host_name=socket.gethostname()
         self.host_ip=socket.gethostbyname(self.host_name)
-    def contact_server(self,num,msg_list):
+        
+    def contact_server(self,num,msg_list):#Establishing connection with the server and interacting with it
         client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
             client.connect((self.server_ip,self.port))
@@ -33,8 +36,9 @@ class Client:
             exit(0)
         
                     
-def parse_message(sentence):
+def parse_message(sentence): 
     return sentence.split(';')
+#config arguments while executing on the terminal
 parser =argparse.ArgumentParser(description="Client 1.1",usage="client.py <flags>")
 parser._print_message(out)
 parser.add_argument("-n","--number",type=int,default=1,help="Number of messages to be sent.")
